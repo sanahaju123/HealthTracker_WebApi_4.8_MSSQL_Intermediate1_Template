efeea2a848e38f4b78681eab9e1a694b769adeb1
sanahaju123 <88725494+sanahaju123@users.noreply.github.com>
2024-01-07 02:10:31 -0800

    Update Configuration.cs

--- HealthTrackerApp/Migrations/Configuration.cs
+++ HealthTrackerApp/Migrations/Configuration.cs
@@ -9,7 +9,7 @@
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(HealthTrackerApp.DAL.DatabaseContext context)
